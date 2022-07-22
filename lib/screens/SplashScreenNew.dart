d0c8c36f256089e3b7c75bf9c05df31b765f69bb
Shemeer Ul Haq <shemeer@skyewall.tech>
2022-07-22 23:30:27 +0530

    Added image icon on splash screen

--- lib/screens/SplashScreenNew.dart
+++ lib/screens/SplashScreenNew.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:machine_test/screens/home_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -7,27 +8,25 @@ import '../provider/drinks_provider.dart';
 class SplashScreenNew extends StatefulWidget {
   const SplashScreenNew({Key? key}) : super(key: key);
 
-  static  final String routeName="/splash";
+  static final String routeName = "/splash";
 
   @override
   _SplashScreenNewState createState() => _SplashScreenNewState();
 }
 
 class _SplashScreenNewState extends State<SplashScreenNew> {
-
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
-    var provider = Provider.of<DrinksProvider>(context,listen: false);
+    var provider = Provider.of<DrinksProvider>(context, listen: false);
 
     provider.getAllDrinks(context);
 
-    Future.delayed(const Duration(seconds: 5),(){
+    Future.delayed(const Duration(seconds: 5), () {
       Navigator.of(context).pushReplacementNamed(HomePage.routeName);
     });
-
   }
 
   @override
@@ -36,7 +35,18 @@ class _SplashScreenNewState extends State<SplashScreenNew> {
       child: SafeArea(
         child: Scaffold(
           body: Center(
-            child: CircularProgressIndicator(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                    height: ScreenUtil().setWidth(126),
+                    width: ScreenUtil().setWidth(169),
+                    child: Image(image: AssetImage('assets/icons/juice.jpeg'))),
+                SizedBox(height: ScreenUtil().setWidth(20)),
+                CircularProgressIndicator(),
+              ],
+            ),
           ),
         ),
       ),
